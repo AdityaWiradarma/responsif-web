2feb9efe1124a347c1760bf539a5eb032a0b9850
AdityaWiradarma <79495596+AdityaWiradarma@users.noreply.github.com>
2021-02-25 23:46:09 +0700

    Add files via upload

---
+++ aweb/index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <link rel:"stylesheet" href="css/style.css" <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="" stylesheet" type="text/css" href="css/style.css">
+    <title>Portofolio</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-info fixed-top">
+        <a class="navbar-brand" href="#">Bande Visual</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tentang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Artikel Terbaru</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+
+    <div class="jumbotron" id="jumbotron">
+        <div class="container">
+            <div class="text-center">
+                <img src="asset\image/profile.jpeg" width="300" class="rounded-circle img-thumbnail">
+                <h1 class="display-4">Bande Visual</h1>
+                <h3 class="lead">Memberikan Informasi Penjualan Desain Grafis Kekinian Di Singaraja</h3>
+            </div>
+        </div>
+    </div>
+
+    <section class="about" id="about">
+        <div class="container">
+            <div class="row mb-4">
+                <div class="col text-center">
+                    <h2>Tentang</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-5 text-justify">
+                    <p>Selamat datang di halaman website ini, halaman di mana menawarkan jasa desain grafis, berupa pembuatan desain edit foto, poster, baju, banner, plakat dan masih banyak lagi dengan harga yang pastinya terjangkau, harga bisa saja berubah sesuai dengan keinginan costumer dan revisinya sebanyak 3x, dan waktu pengerjaantergantung banyaknya pesana, kualitas desain dijamin jelas dan pastinya tidak pecah dan format gambar yaitu png/jpg/jpeg.</p>
+                </div>
+                <div class="col-md-5 text-justify">
+                    <p>Desain grafis adalah suatu seni untuk menyatukan teks dan gambar dalam satu perpaduan yang serasi. Desain grafis sangat berguna dalam menunjang suatu kegiatan, desain yang indah dan menarik akan banyak menarik perhatian masayarakat untuk membeli. Mulai dari edit foto, logo, banner, brosur, spanduk, pamflet dan lain sebagainya.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--Artikel Terbaru-->
+    <section class="Artikel Terbaru bg-light" id="Artikel Terbaru">
+        <div class="container">
+            <div class="row pt-5 mb-4">
+                <div class="col text-center">
+                    <h2>Artikel Terbaru</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md mb-4 text-justify">
+                    <div class="card">
+                        <img class="card-img-top" src="asset/image/artikel2.jpeg" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Hai generasi millenial👋
+                                Kalian mau nyari hadiah untuk org terdekat? Atau untuk diri sendiri?
+                                Kalian mau desain tapi gatau dimana?
+                                Tenang ya gais, kini Bande Visual hadir memberikan solusi terbaik buat kalian.
+
+                                Kalian akan mendapatkan kualitas dan pelayanan terbaik, karena kesenangan anda menjadi tujuan kami.
+
+                                Let's Order!!
+
+                                Best Regards
+                                Bande_Visual</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md mb-4 text-justify">
+                    <div class="card">
+                        <img class="card-img-top" src="asset/image/artikel.jpeg" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Hai generasi millenials👋
+                                Kalian mau nyari hadiah untuk orang terdekat? Atau untuk diri sendiri?
+                                Kalian mau desain tapi gatau dimana?
+                                Tenang ya gais, kini Bande Visual hadir memberikan penawaran spesial nih!!
+
+                                1. Edit foto
+                                2. Desain poster
+                                3. Desain Banner
+                                4. Desain Baju
+                                5. Dan desain lainnya
+
+                                Let's Order!!
+                                Stay tuned terus ya, dan dapatkan penawaran spesial dari kami✨
+
+                                Best Regards
+                                Bande_Visual</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md mb-4 text-justify">
+                    <div class="card">
+                        <img class="card-img-top" src="asset/image/artikel3.jpeg" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Hai generasi millenial👋
+                                Klian mau nyari hadiah untuk org terdekat? Atau untuk diri sendiri?
+                                Kalian mau desain tapi gatau dimana?
+                                Tenang ya gais, kini Bande Visual hadir memberikan solusi terbaik buat kalian.
+
+                                Kalian akan mendapatkan kualitas dan pelayanan terbaik, karena kesenangan anda menjadi tujuan kami.
+
+                                Let's Order!!
+
+                                Best Regards
+                                Bande_Visual</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--footer-->
+    <footer class="bg-dark fixed-bottom text-white mt-5">
+        <div class="container">
+            <div class="row pt-3">
+                <div class="col text-center">
+                    <p>2021 @bande_visual</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <section id="contact" class="contact mb-5">
+        <div class="container">
+            <div class="row pt-4 mb-4">
+                <div class="col text-center">
+                    <h2>Contact Us</h2>
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col=lg-4">
+                    <div class="card text-white bg-primary mb-3 text-center">
+                        <div class="card-body">
+                            <h5 class="card-title">Contact Us</h5>
+                            <p class="card-text">Klik : bit.ly/Bande_Visual atau bisa cek di istagram bande_visual</p>
+                        </div>
+                    </div>
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <h1>Location</h1>
+                        </li>
+                        <li class="list-group-item">Alamat</li>
+                        <li class="list-group-item">Jl. Jalak Putih, Banyuasri</li>
+                        <li class="list-group-item">Bali, Indonesia</li>
+                        <li class="list-group-item">087865633984</li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-6">
+                    <form>
+                        <div class="form-group">
+                            <label for="Nama">Nama</label>
+                            <input type="text" class="form-control" id="Nama">
+
+                        </div>
+                        <div class="form-group">
+                            <label for="Email">Email</label>
+                            <input type="text" class="form-control" id="Email">
+
+                        </div>
+                        <div class="form-group">
+                            <label for="Telepon">Telepon</label>
+                            <input type="text" class="form-control" id="Telepon">
+
+                        </div>
+                        <div class="form-group">
+                            <label for="Pesan">Pesan</label>
+                            <textarea name="Pesan" id="Pesan" class="form-control"></textarea>
+
+                        </div>
+                        <div class="form-group">
+                            <button type="button" class="btn btn-primary">Kirim Pesan</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+</body>
+
+</html>
